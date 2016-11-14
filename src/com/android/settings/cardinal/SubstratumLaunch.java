iskandar1023/packages_apps_Settings

Add Substratum dashboard tile
PS2: Change package detection logic

src/com/android/settings/velvet/SubstratumLaunch.java
@@ -1,0 +1,31 @@
+package com.android.settings.velvet;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
+public class SubstratumLaunch extends SettingsPreferenceFragment {
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsEvent.APPLICATION;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent i = new Intent();
+        PackageManager pm = getActivity().getPackageManager();
+        i = pm.getLaunchIntentForPackage("projekt.substratum");
+        i.addCategory(Intent.CATEGORY_LAUNCHER);
+        startActivity(i);
+        finish();
+    }
+}

Wisllley/packages_apps_Settings-6

Change-Id: Iefe59e5cce93fa1c0f55b89a9b0b3682bda4d917
Conflicts:
	AndroidManifest.xml
	res/values/custom_strings.xml
	src/com/android/settings/SettingsActivity.java

src/com/android/settings/cardinal/SubstratumLaunch.java
@@ -1,0 +1,31 @@
+package com.android.settings.cardinal;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
+public class SubstratumLaunch extends SettingsPreferenceFragment {
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsEvent.APPLICATION;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent i = new Intent();
+        PackageManager pm = getActivity().getPackageManager();
+        i = pm.getLaunchIntentForPackage("projekt.substratum");
+        i.addCategory(Intent.CATEGORY_LAUNCHER);
+        startActivity(i);
+        finish();
+    }
+}